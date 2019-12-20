7609968d257a3715ad5d64eea11968cccd7e91c9
gyyHappy <244160428@qq.com>
2019-12-20 14:18:20 +0800

    将github授权登录信息封装

--- src/main/java/com/gyy/community/controller/AuthorizeController.java
+++ src/main/java/com/gyy/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.gyy.community.dto.AccessTokenDTO;
 import com.gyy.community.dto.GitHubUser;
 import com.gyy.community.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,14 +19,23 @@ public class AuthorizeController {
     @Autowired
     private GitHubProvider gitHubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String uri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("14ce1bdacd596e4269ba");
-        accessTokenDTO.setClient_secret("19254c78b6b2b7ebb885d5168e6d75c79f5d1d5d");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(uri);
         accessTokenDTO.setState(state);
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         GitHubUser user = gitHubProvider.getUser(accessToken);
